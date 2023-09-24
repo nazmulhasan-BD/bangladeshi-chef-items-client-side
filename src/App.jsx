cb18999b76ad9c2fe96ec3857fadf704e70bc353
nazmulhasan-BD <munnanazmu72@gmail.com>
2023-09-24 12:22:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+    return (
+        <div className='text-5xl font-extrabold'>
+           <p className='font-lobstar'> hello Youtube !!!</p>
+            <button className="btn btn-info">Info</button>
+            <button className="btn btn-success">Success</button>
+            <button className="btn btn-warning">Warning</button>
+            <button className="btn btn-error">Error</button>
+        </div>
+    );
+};
+
+export default App;
